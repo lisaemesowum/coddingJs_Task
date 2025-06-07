ee67790514630bef5e178dbb44edd36ec4a13386
lisaemesowum <lisaemesowum8@gmail.com>
2025-06-07 22:06:35 +0100

    add

---
+++ inOrder.js
@@ -0,0 +1,19 @@
+// // Logic-1 -- inOrder
+// Given three ints, a b c, return true if b is greater than 
+// a, and c is greater than b. However, with the exception that if "bOk" is true,
+//  b does not need to be greater than a.
+
+// Examples
+
+// inOrder(1, 2, 4, false) → true
+// inOrder(1, 2, 1, false) → false
+// inOrder(1, 1, 2, true) → true
+
+function inOrder(a,b,c, bOk){
+ if(bOk===false && b > a && c> b){
+    return true;
+ }else if(bOk === true && c > b ){
+     return true
+ }return false
+   
+}
