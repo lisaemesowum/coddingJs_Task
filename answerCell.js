013cdbff18872f652c4de87d59bb8f941109ceee
lisaemesowum <lisaemesowum8@gmail.com>
2025-06-07 16:11:11 +0100

    new

---
+++ answerCell.js
@@ -0,0 +1,22 @@
+// Logic-1 -- answerCell
+// Your cell phone rings. Return true if you should answer it. Normally you
+//  answer, except in the morning you only answer if it is your mom calling.
+//   In all cases, if you are asleep, you do not answer.
+
+// Examples
+
+// answerCell(false, false, false) → true
+// answerCell(false, false, true) → false
+// answerCell(true, false, false) → false
+
+function answerCell(isMorning, isMom, isAsleep){
+    if(isMorning){
+        return false;
+    }
+    if(isMorning && isMom){
+        return false;
+    }
+    else{
+        return true
+    }
+}
