fc6fcc8ba54df42a0e4f4ac0bc168e66118f0fbc
lisaemesowum <lisaemesowum8@gmail.com>
2025-06-10 14:17:23 +0100

    changed

--- zroFront.js
+++ zroFront.js
@@ -11,19 +11,23 @@
 // zeroFront([1, 0]) → 0,1
 
 function zeroFront(nums){
-let newarr=[]
+let newarr=[]                //This will store the rearranged elements.
 // using a for loop
-for(let i = 0; i<nums.length; i++){
+for(let i = 0; i<nums.length; i++){             //First loop: Collect all zeros   //Iterates through each element in nums.
     // using if statement
     if(nums[i]==0){
-        newarr.push(0);
+        newarr.push(0);          //If an element is 0, it gets pushed into newArr.
     }
-}
-    for(let i =0; i<nums.length; i++){
-        if(nums[i]!==0){
-            newarr.push(nums[i])
-        }
-    }return newarr
+    //After this loop, newArr contains all the zeros from nums in their original order.
+} 
+
+
+  //Second loop: Collect all non-zero elements
+    for(let i =0; i<nums.length; i++){             //Iterates through nums again.
+        if(nums[i]!==0){                           //If an element is not [!] 0, it gets pushed into newArr.
+            newarr.push(nums[i]) 
+        }                                         //After this loop, newArr has all the zeros first, followed by the non-zero elements in their original order.
+    }return newarr          //The final array is returned with zeros at the front.
 }
 console.log(zeroFront([1, 0, 0, 1]))
 
